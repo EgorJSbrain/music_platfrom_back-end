95d9288f8498ce1bddf3b24b55b5734cecdc4b45
Egor Pobylets <egor.pobylets@cogniteq.com>
2023-07-06 07:46:30 +0200

    feat: upload files: audio, image

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { TrackModule } from './track/track.module';
+import { FileModule } from './file/file.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import * as path from 'path';
 
 @Module({
   imports: [
@@ -9,6 +12,10 @@ import { TrackModule } from './track/track.module';
     MongooseModule.forRoot(
       `mongodb+srv://user:user@cluster0.igskz4u.mongodb.net/?retryWrites=true&w=majority`,
     ),
+    FileModule,
+    ServeStaticModule.forRoot({
+      rootPath: path.resolve(__dirname, 'static'),
+    }),
   ],
 })
 export class AppModule {}
