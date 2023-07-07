5d996e39455d31e243c1d908315a33642988fe7d
Egor Pobylets <egor.pobylets@cogniteq.com>
2023-07-07 08:00:54 +0200

    feat: add auth, usrs

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsString, Length } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString({ message: 'Email should be a string' })
+  @IsEmail({}, { message: 'Form of email is not correct' })
+  readonly email: string;
+
+  @IsString({ message: 'Password should be a string' })
+  @Length(4, 16, {
+    message:
+      'Password should be more than 4 symbols and not more then 16 symbols',
+  })
+  readonly password: string;
+}
---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+
+@Schema()
+export class User {
+  @Prop()
+  id: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  banned: boolean;
+
+  @Prop()
+  banReason: boolean;
+
+  @Prop({ type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Role' }] })
+  roles: any[]; // TO DO add roles
+
+  // TO DO add posts
+  // TO DO add tracks
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
