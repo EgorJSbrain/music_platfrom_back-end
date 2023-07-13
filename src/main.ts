c2658fcc7486027d13445c5e9cc7f87f66ef002f
Egor Pobylets <egor.pobylets@cogniteq.com>
2023-07-13 08:02:40 +0200

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ const start = async () => {
   try {
     const PORT = process.env.PORT || 5500;
     const app = await NestFactory.create(AppModule);
-
+    app.enableCors();
     await app.listen(PORT, () => console.log('SERVER WORKS'));
   } catch (e) {
     console.log(e);
